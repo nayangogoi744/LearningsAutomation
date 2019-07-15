cda5d422c86415cf304e71a34a7f03d9ac8d68d6
nayangogoi744 <nayangogoi744>
2019-07-15 16:23:24 +0530

    Final

--- src/test/java/com/testtalks/karatedemo/MavenProjectSelenium/AppTest.java
+++ src/test/java/com/testtalks/karatedemo/MavenProjectSelenium/AppTest.java
@@ -26,26 +26,30 @@ public class AppTest {
 	
 	@Test(dataProvider="providelogininfo")
 	public void login(String username,String password) throws InterruptedException, MalformedURLException{
-
+		//String exePath = "C:\\Old-data\\programming\\chromedriver_win32-75\\chromedriver.exe";
+	//	System.setProperty("webdriver.chrome.driver", exePath);
 		 WebDriverManager.chromedriver().setup();
 
 		ChromeOptions options = new ChromeOptions();
-
+		//disable automation info bar
+		//options.addArguments("--disable-gpu");
+		//options.addArguments("--no-sandbox");
 		driver =  new ChromeDriver(options);
 	       
-
+		   //WebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:9515"), DesiredCapabilities.chrome());
+		   //WebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:9515"), DesiredCapabilities.chrome());
 	       driver.get("https://s1.demo.opensourcecms.com/wordpress/wp-login.php");
 	       driver.findElement(By.xpath("//input[@id='user_login']")).sendKeys(username);
 	       driver.findElement(By.xpath("//input[@id='user_pass']")).sendKeys(password);
 	       driver.findElement(By.xpath("//input[@id='wp-submit']")).click();
 	       Thread.sleep(2000);
-	       System.out.println(driver.getTitle());
-	       String title = driver.getTitle();
+	       //System.out.println(driver.getTitle());
+	       boolean title = driver.getTitle().contains("Dashboard");
 	     
 	      
 	     //  Assert.assertTrue(driver.getTitle().contains("Dashboard"),"Unable to login using the provided info");
-	       Assert.assertEquals("Dashboard ‹ opensourcecms — WordPress", title);	  
-	       //Assert.assertTrue(title);
+	       //Assert.assertEquals("Dashboard ‹ opensourcecms — WordPress", title);	  
+	       Assert.assertTrue(title);
 	       System.out.println("Successfully login");
 	      
 	}
